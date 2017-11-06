790e532bcdc88202788572f731071b8cbb2c6a72
Isaac Park <isaac.a.park@gmail.com>
2017-11-05 22:52:19 -0500

    Send email

--- src/start.ts
+++ src/start.ts
@@ -1,12 +1,23 @@
-import * as nodemailer from "nodemailer";
+const sendmail = require('sendmail')();
 import * as Express from "express";
 import * as bodyParser from "body-parser";
 
 const app = Express();
 
 app.use(bodyParser.urlencoded());
+app.use(Express.static('.'))
+
 
 app.post('/api/register', function(req, res) {
+    sendmail({
+        from: 'sender@lab.isaacpark.me',
+        to: req.body.email,
+        subject: 'Registered for notifications',
+        html: 'You appear to have registered to be notified to any changes to the output of '+req.body.url,
+    }, function(err: any, reply: any) {
+        console.log(err && err.stack);
+        console.dir(reply);
+    });
     res.redirect('/success.html?='+req.body.email);
 });
 
