685fe0e5b0eaf0263819885df37b9a2a0cfb3f54
Isaac Park <isaac.a.park@gmail.com>
2017-11-06 09:54:08 -0500

    Send notifications

--- src/start.ts
+++ src/start.ts
@@ -1,12 +1,51 @@
 const sendmail = require('sendmail')();
 import * as Express from "express";
 import * as bodyParser from "body-parser";
+import * as request from "request";
+import fetch from "node-fetch";
 
 const app = Express();
 
 app.use(bodyParser.urlencoded());
 app.use(Express.static('.'))
 
+setInterval(() => {
+    listeners.forEach(l => setTimeout(() => l.tick(), Math.random()*1000*60*5));
+}, 1000*60*17)
+
+class ApiListener {
+    url: string;
+    email: string;
+    lastValue: string|null = null;
+    constructor(url: string, email: string) {
+        this.url = url;
+        this.email = email;
+    }
+
+    async pull() {
+        let result = await fetch(this.url);
+        let value = result.body.read().toString();
+        let changed = value === this.lastValue;
+        this.lastValue = value;
+        return changed;
+    }
+
+    sendUpdate() {
+        sendmail({
+            from: 'sender@lab.isaacpark.me',
+            to: this.email,
+            subject: 'Important, the API has changed.',
+            html: `${this.url} has changed to ${this.lastValue}`,
+        }, function(err: any, reply: any) {});
+    }
+
+    tick() {
+        this.pull()
+            .then(r => r?this.sendUpdate():null);
+    }
+}
+
+let listeners: ApiListener[] = [];
 
 app.post('/api/register', function(req, res) {
     sendmail({
@@ -18,6 +57,8 @@ app.post('/api/register', function(req, res) {
         console.log(err && err.stack);
         console.dir(reply);
     });
+    listeners.push(new ApiListener(req.body.url, req.body.email));
+
     res.redirect('/success.html?='+req.body.email);
 });
 
