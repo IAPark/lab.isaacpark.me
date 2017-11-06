0b02a447452440c948d64dc1d1b707bb788481d7
Isaac Park <isaac.a.park@gmail.com>
2017-11-05 22:24:07 -0500

    Parse body?

--- src/start.ts
+++ src/start.ts
@@ -1,8 +1,10 @@
 import * as nodemailer from "nodemailer";
 import * as Express from "express";
+import * as bodyParser from "body-parser";
 
 const app = Express();
 
+app.use(bodyParser.urlencoded());
 
 app.post('/api/register', function(req, res) {
     res.redirect('/success.html?='+req.body.email);
